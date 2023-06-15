8fd0216b7c3d42dee738042c7cf6e4e65725a252
Phillip Sussman <phillipsussman@gmail.com>
2023-06-15 11:58:59 -0700

    fixed text

--- client/src/App.js
+++ client/src/App.js
@@ -35,14 +35,14 @@ const App = () => {
 			{!authToken && <Auth />}
 			{authToken && (
 				<>
-					<ListHeader listName={"🏝️Holiday tick list"} getData={getData} />
+					<ListHeader listName={"📠ToDo list"} getData={getData} />
 					<p className="user-email">Welcome back {userEmail}</p>
 					{sortedTasks?.map((task) => (
 						<ListItem key={task.id} task={task} getData={getData} />
 					))}
 				</>
 			)}
-			<p className="copyright">© Creative Coding LLC</p>
+			<p className="copyright">© Phillip Sussman</p>
 		</div>
 	);
 };
